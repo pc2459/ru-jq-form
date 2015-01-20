fa14eb5b3137d0bf5d1e70d084087d80c58351ec
Po Linn Chia <chiapolinn@gmail.com>
2015-01-20 11:34:04 -0700

    Get form to work

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 $(document).on('ready', function() {
+
+	$( "#form" ).submit(function(e){
+		console.log("Form submitted");
+
+		$("#name").text($("#form-name").val());
+		$("#bio").text($("#form-bio").val());
+		$("#books").text($("#form-books").val());
+		$("#libraries").text($("#form-libraries").val());
+
+		e.preventDefault();
+	})
   
 });
